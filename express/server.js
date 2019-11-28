624e532b6f66c6550ba3132aae79341e3ce69c05
David Arfine <darfine@primeline.com>
2019-11-28 07:42:14 -0500

    update

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/dit', (req, res) => {
+router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello Dave!</h1>');
   res.end();
